0fa0941581862507a570374885f6d6e72438a167
Ron <Basinr@gmail.com>
2016-03-04 22:58:56 -0500

    obtain token, try again

--- center.py
+++ center.py
@@ -13,24 +13,26 @@ def index():
 @app.route('/cakes/')
 def cakes():
 	# OCRparse.main("xoxp-13657523393-23584016902-23864788196-fed69d1b0a")
-	code = request.args['code']
 
-	print code
-	token = OCRparse.get_access_token(code)
+	if (len(request.args) == 1:
+		code = request.args['code']
+
+		print code
+		token = OCRparse.get_access_token(code)
 
 	# 	print "#######################################error"
 	# token: 'xoxp-13657523393-23584016902-24270415890-381512abb5'
-	print token
-	print OCRparse.start(token)
+		print token
+		print OCRparse.start(token)
 	return render_template('success.html')
 	# OCRparse.start('xoxp-13657523393-23584016902-24270415890-381512abb5')
 	# 'xoxp-13657523393-23584016902-24270415890-381512abb5' + "####################################"
 	# return render_template('index.html')
 
-@app.route('/cakes/booty/')
-def cakes_booty():
-	# OCRparse.main("xoxp-13657523393-23584016902-23864788196-fed69d1b0a")
-	return render_template('index.html')
+# @app.route('/cakes/booty/')
+# def cakes_booty():
+# 	# OCRparse.main("xoxp-13657523393-23584016902-23864788196-fed69d1b0a")
+# 	return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
