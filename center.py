bad8dfd7a3031800aa20acf5734ddadd543b58a1
Gary Basin <gbasin@gmail.com>
2016-03-12 01:13:04 -0600

    debug code

--- center.py
+++ center.py
@@ -86,10 +86,12 @@ def signup():
 def cpanel():
 	pw = request.args.get('pw')
 
+	print '1'
 	if pw != 'growingballer89!':
 		return index()
-
+	print '2'
 	users = get_users()
+	print '3'
 	print json.dumps(users)
 	return render_template('cpanel.html', users=get_users())
 
