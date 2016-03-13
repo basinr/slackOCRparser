6e8cd4a5fa6cde2a797578fe347e7544567299fa
Gary Basin <gbasin@gmail.com>
2016-03-13 16:14:57 -0500

    typo

--- center.py
+++ center.py
@@ -114,5 +114,4 @@ def cpanel():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-	app.debug = True #temp
     app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
