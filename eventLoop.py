a853c52de41a3c9ebef5ac7225eed0106a1eb3f8
Gary Basin <gbasin@gmail.com>
2016-03-13 17:33:55 -0500

    refactor

--- eventLoop.py
+++ eventLoop.py
@@ -1,39 +1,41 @@
-import OCRparse
 import threading
 import time
+import center
 
 
-eventLoopStopFlag = None
-eventLoopThread = None
+EventLoopStopFlag = None
+EventLoopThread = None
 
 
 def start_event_loop():
-	global eventLoopThread
-	global eventLoopStopFlag
+	global EventLoopThread
+	global EventLoopStopFlag
 
-	if eventLoopThread and not (eventLoopThread is None) and eventLoopThread.is_alive():
+	if EventLoopThread and not (EventLoopThread is None) and EventLoopThread.is_alive():
 		print "Event loop already running"
 		stop_event_loop()
 
-	eventLoopStopFlag = threading.Event()
-	eventLoopThread = threading.Thread(target=event_loop, args=(eventLoopStopFlag,))
-	eventLoopThread.start()
+	EventLoopStopFlag = threading.Event()
+	EventLoopThread = threading.Thread(target=event_loop, args=(EventLoopStopFlag,))
+	EventLoopThread.start()
 	print "Event loop thread started!"
 
 
 def stop_event_loop():
-	global eventLoopThread
-	global eventLoopStopFlag
+	global EventLoopThread
+	global EventLoopStopFlag
 
 	print "Terminating event loop..."
-	eventLoopStopFlag.set()
-	eventLoopThread.join()
+	EventLoopStopFlag.set()
+	EventLoopThread.join()
 	print "Terminated!"
 
 
 def event_loop(stop_flag):
 	while not stop_flag.is_set():
 		# get Users dict
+		users_dict = center.get_users()
+
 		# check for updates
 		# sleep
 		time.sleep(1)
