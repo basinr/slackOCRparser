fd56e303d9e05a5f4d0e3e7361dd0f453f3d5e5c
Gary Basin <gbasin@gmail.com>
2016-03-13 19:31:07 -0500

    int to str

--- eventLoop.py
+++ eventLoop.py
@@ -39,7 +39,7 @@ def event_loop(stop_flag):
 
 			# check for updates
 			for key, user in users_dict.iteritems():
-				print key + ' ' + user
+				print str(key) + ' ' + user
 
 			# sleep
 			time.sleep(1)
