bff42c9549b012f383ddf5ad2aa439c1dcfd6a6a
Gary Basin <gbasin@gmail.com>
2016-03-15 18:39:22 -0500

    syntax fuc

--- eventLoop.py
+++ eventLoop.py
@@ -10,6 +10,7 @@ from python_slackclient.slackclient import SlackClient
 EventLoopStopFlag = None
 EventLoopThread = None
 
+
 def is_service_active():
 	if EventLoopThread and not (EventLoopThread is None) and EventLoopThread.is_alive():
 		return True
@@ -20,7 +21,7 @@ def start_event_loop():
 	global EventLoopThread
 	global EventLoopStopFlag
 
-	if is_service_active()
+	if is_service_active():
 		print "Event loop already running"
 		stop_event_loop()
 
