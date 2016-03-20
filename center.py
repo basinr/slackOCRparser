2001f4d1f8b40efcdf917a725cbd3d0f9da78eed
Gary Basin <gbasin@gmail.com>
2016-03-20 14:58:38 -0500

    trying without Debug

--- center.py
+++ center.py
@@ -160,4 +160,4 @@ if __name__ == "__main__":
 		slack_thread_mgr = slackThread.SlackThreadManager()
 
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
+	app.run(host='0.0.0.0', port=port, threaded=True, debug=False)
