59b2e0d863cd4c5302cd4216a2a6fea8110067d0
Gary Basin <gbasin@gmail.com>
2016-04-02 21:50:36 -0500

    Adding help menu

--- client_mgr/bot.py
+++ client_mgr/bot.py
@@ -3,4 +3,19 @@ def process(bot_user_id, text):
 	if bot_user_id not in text:
 		return False
 
-	return "123"
+	if text == "help":
+		return help_menu()
+	elif text == "start":
+		return "OCR enabled!"
+	elif text == "stop":
+		return "OCR disabled!"
+	elif text == "account":
+		return "account info TODO"
+
+
+def help_menu():
+	menu = "\n `start` to enable bot" \
+		+ "\n `stop` to disable bot" \
+		+ "\n `account` to see account info"
+
+	return menu
